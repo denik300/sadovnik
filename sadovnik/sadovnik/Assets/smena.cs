5fc37aa84d3511130c03175377208e5494ce567b
sadovnik1231 <43670196+Riperia@users.noreply.github.com>
2020-12-13 15:08:55 +0200

    Initial commit

---
+++ sadovnik/sadovnik/Assets/smena.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class smena : MonoBehaviour
+{
+    public void nachalok(string scene_name)
+    {
+        // Application.LoadLevel(Application.loadedLevel);
+        SceneManager.LoadScene(scene_name);
+
+    }
+}
